c37872a8ec38667bfbbf72110ef9c3f187197c32
vmikov <vmikov@ukr.net>
2019-06-26 12:02:26 +0300

    builds for production

---
+++ build/precache-manifest.8eaf6e44774d1a319572c229c4929c0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "042f7557082b6c47d51f76df01503178",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "e4a3626bf46827b7dc22",
+    "url": "/calendar/static/css/main.11664988.chunk.css"
+  },
+  {
+    "revision": "dcc3f774d05cc4b15bcd",
+    "url": "/calendar/static/js/2.16fcc057.chunk.js"
+  },
+  {
+    "revision": "e4a3626bf46827b7dc22",
+    "url": "/calendar/static/js/main.19a7fc1d.chunk.js"
+  },
+  {
+    "revision": "70cd18d5f6522f88cc6f",
+    "url": "/calendar/static/js/runtime~main.3b46a14a.js"
+  }
+]);
